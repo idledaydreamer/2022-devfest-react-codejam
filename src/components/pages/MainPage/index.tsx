f144aea325ad14b05a32bffb407de3f5e5055cd5
Yehyun <idleday@mirabellsoft.com>
2022-12-17 23:49:22 +0900

    :tada: 2022-devfest-react-codejam

--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,8 +1,40 @@
+import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { ActivityComponentType } from '@stackflow/react';
 import React from 'react';
+import Footer from 'src/components/common/Footer';
+import ProductItem from 'src/components/common/ProductItem';
+import { MainPageAppBarLeft } from 'src/components/common/Stackflow';
+import { ProductInterface } from 'src/schemas/Product';
+import { getProductList } from 'src/services/product';
+import { useFlow } from 'src/utils/stackflow';
 
 const MainPage: ActivityComponentType = () => {
-  return <div></div>;
+  const { push } = useFlow();
+  const [products, setProducts] = React.useState<ProductInterface[]>();
+
+  const goToDetailPage = (id: number) => {
+    push('DetailPage', { id: id.toString() });
+  };
+
+  const loopProducts = async () => {
+    const { data } = await getProductList();
+    setProducts(data);
+    console.log(products);
+  };
+
+  React.useEffect(() => {
+    loopProducts();
+  }, []);
+
+  return (
+    <AppScreen appBar={{ appendLeft: MainPageAppBarLeft }}>
+      {products &&
+        products.map((product) => (
+          <ProductItem key={product.id} item={product} onClickItem={() => goToDetailPage(product.id)} />
+        ))}
+      <Footer />
+    </AppScreen>
+  );
 };
 
 export default MainPage;
